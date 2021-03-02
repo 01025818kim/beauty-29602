80742d67b70ff6e9d92f68b55dcb9b6f1306133f
01025818kim <hime.09.22kim@icloud.com>
2021-03-03 08:23:49 +0900

    Fakerの導入

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :user do
-    first_name            {'テスト'}
-    last_name             {'太郎'}
-    nick_name             {'test'}
-    email                 {'test@example'}
-    password              {'000000km'}
+    first_name            {Faker::Name.first_name}
+    last_name             {Faker::Name.last_name}
+    nick_name             {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
   end
 end
