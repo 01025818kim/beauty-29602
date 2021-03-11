6ef7017ddf27e875b81eab6e30a56a33d6c6eec2
01025818kim <hime.09.22kim@icloud.com>
2021-03-12 08:33:49 +0900

    articleテーブルの作成

--- db/migrate/20210311232040_create_articles.rb
+++ db/migrate/20210311232040_create_articles.rb
@@ -1,7 +1,11 @@
 class CreateArticles < ActiveRecord::Migration[6.0]
   def change
     create_table :articles do |t|
-      
+      t.references :user,         null: false,foreign_key: true
+      t.string     :title,        null: false
+      t.integer    :genre_id,     null: false
+      t.integer    :category_id,  null: false
+      t.text       :text,         null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_28_222946) do
+ActiveRecord::Schema.define(version: 2021_03_11_232040) do
+
+  create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "title", null: false
+    t.integer "genre_id", null: false
+    t.integer "category_id", null: false
+    t.text "text", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "first_name", null: false
@@ -27,4 +38,5 @@ ActiveRecord::Schema.define(version: 2021_02_28_222946) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "articles", "users"
 end
