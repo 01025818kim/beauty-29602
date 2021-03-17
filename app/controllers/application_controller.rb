08d4b9cd47e9e7b70fa5629fe3de50e736b406f8
01025818kim <hime.09.22kim@icloud.com>
2021-03-18 08:41:03 +0900

    ユーザー管理機能の修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(
-      :sign_up, keys: [:firstname, :lastname, :nickname]
+      :sign_up, keys: [:first_name, :last_name, :nick_name]
     )
   end
 end
